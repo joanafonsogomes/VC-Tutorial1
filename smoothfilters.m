c3875100d8f5b1731343a8543815214f2336492a
Filipe Guimarães <fmtfg99@gmail.com>
2020-12-07 19:58:32 +0000

    avanços

--- smoothfilters.m
+++ smoothfilters.m
@@ -1,23 +1,50 @@
 original = 'baboon';
 ficheiro = 'baboon.png';
 
-ruido = 'gaussian';
-paramRuido = [0,0.01];
+ruido = 'salt & pepper';
+paramRuido = [0.02,0.02];
+%Para salt & pepper usar o primeiro valor
+%para gaussian 1º valor para a média e o 2º para a variância
 
-dominioFiltro = 'frequency';
-tipoSmoothing = 'butterworth';
-paramFiltro = [10,4,10];
+dominioFiltro = 'spatial';
+tipoSmoothing = 'median';
+paramFiltro = [100,100,0];
+%Para dominioFiltro spacial:
+%average - Usado apenas o primeiro valores para definir o tamanho da matriz
+%gaussian - Usado o primeiro valor para o tamanho da matriz, e o segundo valor para o sigma
+%median - Usado o primeiro valor para o numero de colunas da matriz e o segundo para as linhas
+%Para frequencia:
 
-%%%%%%%%%%%%%%%%%%%%%%%%5
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%
 
 imagem = rgb2gray(imread(ficheiro));
-imagem = im2double(imagem);%/255;
+%imagem = double(imagem);%/255;
 [noise,smooth] = main_smoothfilters(imagem,ruido,paramRuido,dominioFiltro, tipoSmoothing, paramFiltro);
 
-output = strcat(original,'_',dominioFiltro,'_',tipoSmoothing,'_',num2str(paramFiltro),'.png');
+output = 'error';
+switch dominioFiltro
+    case 'spatial'
+        switch tipoSmoothing
+            case 'average'
+                output = strcat(original,'_',dominioFiltro,'_',tipoSmoothing,'_',num2str(paramFiltro(1)),'.png');
+            case 'gaussian'
+                output = strcat(original,'_',dominioFiltro,'_',tipoSmoothing,'_',num2str(paramFiltro(1)),'_',num2str(paramFiltro(2)),'.png');
+            case 'median'
+                output = strcat(original,'_',dominioFiltro,'_',tipoSmoothing,'_',num2str(paramFiltro(1)),'_',num2str(paramFiltro(2)),'.png');
+        end
+    case 'frequency'
+end
+  
 imwrite(smooth,output);
 
-output = strcat(original,'_',ruido,'_',num2str(paramFiltro),'_','.png');
+if(strcmp(ruido,'salt & pepper'))
+    output = strcat(original,'_',ruido,'_',num2str(paramRuido(1)),'.png');
+else
+    output = strcat(original,'_',ruido,'_',num2str(paramRuido(1)),'_',num2str(paramRuido(2)),'_','.png');
+end
+
 imwrite(noise,output);
 
 
